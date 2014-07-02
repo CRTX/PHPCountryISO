ff6164d1ccf3d3cd77d91089a256a697801a09f8
CRTX <ruiz.d.christian@gmail.com>
2014-07-02 16:41:44 -0500

    Added test countries.

--- test/Factory/FactoryTest.php
+++ test/Factory/FactoryTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use CRTX\CountryISO\Entity\EntityFactory;
+use Symfony\Component\Yaml\Parser;
 
 class FactoryTest extends \PHPUnit_Framework_TestCase
 {
@@ -9,5 +10,14 @@ class FactoryTest extends \PHPUnit_Framework_TestCase
         $EntityFactory = new EntityFactory();
         $Country = $EntityFactory->build('Country');
         $this->assertInstanceOf('CRTX\\CountryISO\\Entity\\Country', $Country);
+    }
+
+    public function testParser()
+    {
+        $yaml = new Parser();
+
+        $array = $yaml->parse(file_get_contents(__DIR__ . '/../../countries.yml'));
+
+        $this->assertEquals(key($array), 'Country');
     }
 }
