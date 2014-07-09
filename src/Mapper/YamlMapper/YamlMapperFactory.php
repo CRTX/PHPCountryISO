5c51ed1f3fa9890cb80d3bd7bdb1e9a03c18aeba
CRTX <ruiz.d.christian@gmail.com>
2014-07-09 15:28:26 -0500

    Removed ability to parse Yaml. This should be done by a yaml parser along with file_get_contents() and then passed to the build function. This prevents from making redundant tests of checking for proper yaml parsing when these tests should already be done by the library.

--- src/Mapper/YamlMapper/YamlMapperFactory.php
+++ src/Mapper/YamlMapper/YamlMapperFactory.php
@@ -4,11 +4,11 @@ namespace CRTX\CountryISO\Mapper\YamlMapper;
 
 use CRTX\CountryISO\Factory\AbstractFactory;
 use CRTX\CountryISO\Entity\EntityFactory;
-use Symfony\Component\Yaml\Parser;
 
 class YamlMapperFactory extends AbstractFactory
 {
     protected $EntityFactory;
+    protected $buildArguments;
 
     public function __construct(EntityFactory $EntityFactory = null)
     {
@@ -21,30 +21,10 @@ class YamlMapperFactory extends AbstractFactory
         $this->namespace = 'CRTX\\CountryISO\\Mapper\\YamlMapper\\';
     }
 
-    public function build($string, array $arguments = array())
+    protected function modifyBuildArguments($arguments)
     {
-        if ($this->validYamlArguments($arguments)) {
-            $yaml = new Parser();
-            $filepath = array_shift($arguments);
-            $yamlToArray = $yaml->parse(file_get_contents($filepath));
-            array_unshift($arguments, $yamlToArray);
-        }
+        $this->buildArguments = $arguments;
 
-        array_unshift($arguments, $this->EntityFactory);
-
-        $reflection = new \ReflectionClass($this->namespace . $string);
-        return $reflection->newInstanceArgs($arguments);
-    }
-
-    protected function validYamlArguments(array & $arguments)
-    {
-        if(is_array($arguments)) {
-            return false;
-        }
-
-        if (!empty($arguments) && file_exists($arguments[0])) {
-            return true;
-        }
-        return false;
+        array_unshift($this->buildArguments, $this->EntityFactory);
     }
 }
